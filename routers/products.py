24a8f6330afff340ab54376770d41f9aaeda57c9
Owen <Xiongwen.Ouyang@edu.sait.ca>
2026-08-27 20:03:00 -0600

    add products models

--- routers/products.py
+++ routers/products.py
@@ -3,5 +3,9 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/api/products", tags=["products"])
 
 @router.get("/")
-async def get_products():
-    return {"msg": "get products info"}
+async def get_products(skip: int=0, limit: int=10):
+    return {
+        "code": 200,
+        "message": "get products successfully",
+        "data": "get products info"
+    }
