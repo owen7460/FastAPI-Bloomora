4980d9cfd0de2927c126adf62422abc92cc2254d
Owen <Xiongwen.Ouyang@edu.sait.ca>
2026-09-01 20:45:40 -0600

    add cors as middleware

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import products
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     return {"message": "Hello from Bloomora"}
